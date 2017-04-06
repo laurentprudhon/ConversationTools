2b202efc261309b2cd268302493b52290be42a62
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2017-04-06 23:55:45 +0200

    At most one entity value match for the same synonym in a given entity

--- dialogtool/EntityValuesMatcher.cs
+++ dialogtool/EntityValuesMatcher.cs
@@ -7,7 +7,7 @@ namespace dialogtool
 {
     class EntityValuesMatcher
     {
-        public static string ReplaceTextWithConcepts(string originalText, IDictionary<string, ConceptGroupWithTheSameSynonym> conceptsSynonyms, Regex conceptsRegex, out IList<ConceptSubstitution> conceptSubstitutions)
+        public static string ReplaceTextWithConcepts(string originalText, IDictionary<string, ConceptGroupWithTheSameSynonym> conceptsSynonyms, Regex conceptsRegex, out IDictionary<int[],ConceptSubstitution> conceptSubstitutions)
         {
             var textReplacedWithConcepts = originalText;
             int replacementOffset = 0;
@@ -16,7 +16,7 @@ namespace dialogtool
             var matches = conceptsRegex.Matches(originalText);
             if (matches.Count > 0)
             {
-                conceptSubstitutions = new List<ConceptSubstitution>();
+                conceptSubstitutions = new Dictionary<int[], ConceptSubstitution>();
                 foreach (var obj in matches)
                 {
                     var match = (Match)obj;
@@ -26,16 +26,28 @@ namespace dialogtool
                     var synonym = match.Value;
                     var replacementConceptGroup = conceptsSynonyms[synonym];
                     string replacementText = null;
+                    int[] valuesIndexesAfterReplacement = null;
                     if (replacementConceptGroup.Concepts.Count() == 1)
                     {
                         replacementText = replacementConceptGroup.Concepts.First().CanonicalValue;
+                        valuesIndexesAfterReplacement = new int[] { startIndex };
                     }
                     else
                     {
+                        valuesIndexesAfterReplacement = new int[1 + replacementConceptGroup.Concepts.Count()];
+                        int i = 0;
+                        int localOffset = startIndex + replacementOffset;
+
                         replacementText = "{" + synonym;
+                        localOffset += 1;
+                        valuesIndexesAfterReplacement[i++] = localOffset;
+                        localOffset += synonym.Length;
                         foreach (var concept in replacementConceptGroup.Concepts)
                         {
                             replacementText += "|" + concept.CanonicalValue;
+                            localOffset += 1;
+                            valuesIndexesAfterReplacement[i++] = localOffset;
+                            localOffset += concept.CanonicalValue.Length;
                         }
                         replacementText += "}";
 
@@ -44,9 +56,10 @@ namespace dialogtool
                     if (replacementText != synonym)
                     {
                         var conceptSubstitution = new ConceptSubstitution(originalText, startIndex, stopIndex, replacementConceptGroup, replacementText);
-                        conceptSubstitutions.Add(conceptSubstitution);
+                        var newStartIndexAfterReplacement = startIndex + replacementOffset;
+                        conceptSubstitutions.Add(valuesIndexesAfterReplacement, conceptSubstitution);
 
-                        var beforeSubstitution = textReplacedWithConcepts.Substring(0, startIndex + replacementOffset);
+                        var beforeSubstitution = textReplacedWithConcepts.Substring(0, newStartIndexAfterReplacement);
                         var afterSubstitution = textReplacedWithConcepts.Substring(stopIndex + replacementOffset + 1, textReplacedWithConcepts.Length - stopIndex - replacementOffset - 1);
                         textReplacedWithConcepts = beforeSubstitution + replacementText + afterSubstitution;
                         replacementOffset += replacementText.Length - (stopIndex - startIndex + 1);
@@ -62,7 +75,7 @@ namespace dialogtool
             // Remove accented characters before the matching process
             var textWithoutAccentedChars = StringUtils.RemoveDiacritics(originalText);
 
-            IList<ConceptSubstitution> conceptSubstitutions;
+            IDictionary<int[],ConceptSubstitution> conceptSubstitutions;
             var textReplacedWithConcepts = ReplaceTextWithConcepts(textWithoutAccentedChars, conceptsSynonyms, conceptsRegex, out conceptSubstitutions);
                         
             IList<EntityValueMatch> entityValueMatches = new List<EntityValueMatch>();
@@ -70,8 +83,39 @@ namespace dialogtool
             foreach (var entity in entities)
             {
                 var matches = entity.EntityValuesRegex.Matches(textReplacedWithConcepts);
+                ISet<int> substitutionsAlreadyMatched = new HashSet<int>();
                 foreach(Match match in matches)
                 {
+                    // If match found after substitution, make sure we only match one concept for a given entity
+                    if(conceptSubstitutions != null)
+                    {
+                        ConceptSubstitution conceptSubstitution = null;
+                        foreach (var valuesIndexesAfterReplacement in conceptSubstitutions.Keys)
+                        {
+                            if(valuesIndexesAfterReplacement[0] > match.Index)
+                            {
+                                break;
+                            }
+                            if (valuesIndexesAfterReplacement.Contains(match.Index))
+                            {
+                                conceptSubstitution = conceptSubstitutions[valuesIndexesAfterReplacement];
+                                break;
+                            }
+                        }
+                        if(conceptSubstitution != null)
+                        {
+                            var originalStartIndex = conceptSubstitution.StartIndex;
+                            if (substitutionsAlreadyMatched.Contains(originalStartIndex))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                substitutionsAlreadyMatched.Add(originalStartIndex);
+                            }
+                        }
+                    }
+
                     var entityValueText = match.Value;
                     var entityValue = entity.TryGetEntityValue(entityValueText);
                     if(entityValue == null)
@@ -84,7 +128,7 @@ namespace dialogtool
                 }
             }
             
-            return new EntityValuesMatchResult(originalText, conceptSubstitutions, textReplacedWithConcepts, entityValueMatches, entityValues);
+            return new EntityValuesMatchResult(originalText, conceptSubstitutions != null ? conceptSubstitutions.Values.ToList() : null, textReplacedWithConcepts, entityValueMatches, entityValues);
         }
     }
 }
